27846f84e457e6e25d3e47d21a7efd20d2fe185e
bijan <bmassoumi@gmail.com>
2024-06-06 00:03:11 -0400

    fox tagline

--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -10,7 +10,10 @@ const Tagline = () => {
       src={Couch} className="headshotBackground" alt="headshot"       
       style={ !isDesktop?{
         objectFit: "cover",
-      }:{}}
+        height: "100vh",
+      }:{
+        
+      }}
       />
       <Col className="content">
         <h1 style={{fontSize: '50pt'}}>Ryan Goodcase</h1>
